45ae772eb7a33572620ed8ba3a94f054b034c6f5
AnshuGahlyan <71934013+AnshuGahlyan@users.noreply.github.com>
2021-07-28 16:09:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var s;
+
+
+
+function preload(){
+s = loadSound("90.mp3");
+ 
+
+}
+function setup() {
+  createCanvas(1340,640);
+  
+ 
+
+}
+
+
+function draw() {
+s.play();
+window.open("https://anshugahlyan.github.io/opss/index.html");
+  background(0);
+  drawSprites();
+
+   }
+
+     
+
+  
